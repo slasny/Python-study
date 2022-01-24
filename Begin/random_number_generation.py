5ebdb5e7833a2cbd3d83e0dfce90c903a4661aac
slasny <49735376+slasny@users.noreply.github.com>
2022-01-24 19:45:30 +0200

    Create random_number_generation.py
    learning how to works package  -random

---
+++ Begin/random_number_generation.py
@@ -0,0 +1,14 @@
+from random import randint
+from random import choice
+
+random_number = randint(1, 100)
+print(random_number)
+
+string = "String"
+random_index = randint(0, len(string) - 1)
+char = string[random_index]
+print(char)
+
+string = "Anather sample string"
+char = choice(string)
+print(char)
