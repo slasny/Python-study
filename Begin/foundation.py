9bebe5e3554c51e5dbe6f05989f45dbaa65c4cb3
slasny <49735376+slasny@users.noreply.github.com>
2022-01-24 15:55:28 +0200

    Create foundation.py
    Repeat foundation of Python

---
+++ Begin/foundation.py
@@ -0,0 +1,19 @@
+string1 = "First string"
+string2 = "Second string"
+
+x = True;
+y = False;
+
+number = 12;
+
+i = 1
+while 0 <= 10:
+    i = i + 1;
+    print(i)
+    if i > 5:
+        break
+
+def get(string, index = 0):
+    return string[index] + " it's symbol of that index"
+
+print(get("Java", 2))        
