3b43defc2770056c0f82b6262ca30b7c62aedad8
slasny <49735376+slasny@users.noreply.github.com>
2022-01-25 17:08:42 +0200

    Create list1.py
    intro  of List

---
+++ List/list1.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3]
+strings = ["foo", "bar"]
+booleans = [True, False]
+empty_list = []
+print(strings)
+
+numbers.append(5) #add one ellement
+numbers.extend([56,78,78]) #add one and more ellements
+print(numbers)
